132fe8df37e23bb329f67a317fc05f2f7c928914
Aashiha Priyadarshni <aashihapl@gmail.com>
2018-01-18 14:49:10 -0800

    compute diameter of BST

---
+++ Trees/20. Diameter_BST.java
@@ -0,0 +1,42 @@
+// Diameter of BST
+
+Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longestpath between any two nodes in a tree. This path may or may not pass through the root.
+Example:
+Given a binary tree 
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+Method 1:
+Maintain t and l for each node. l is max of length of left nodes and right nodes plus 1. t value is the sum of length left nodes and right nodes. max_t will hold the maximum t till now and its value will be returned.
+
+Code:
+class Solution 
+{
+    int max_t = 0;
+    public int diameterOfBinaryTree(TreeNode root) 
+    {
+        len_node(root);    
+        return max_t;
+    }
+    
+    public int len_node(TreeNode root)
+    {
+        if(root == null)
+            return 0;
+        if(root.left == null && root.right == null)
+            return 0;
+        int t = 0;
+        if(root.left == null || root.right == null)
+            t = len_node(root.left) + len_node(root.right) + 1;
+        else
+            t = len_node(root.left) + len_node(root.right) + 2;
+        max_t = (max_t < t) ? t : max_t;
+        int l1 = (len_node(root.left) > len_node(root.right)) ? len_node(root.left) : len_node(root.right);
+        int l = l1 + 1;
+        return l;
+    }
+}
+
