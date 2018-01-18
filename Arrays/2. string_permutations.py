b21a7cc1d0605c62df19989dfa6ed0a7f3aaf5ac
Aashiha Priyadarshni <aashihapl@gmail.com>
2018-01-18 14:57:37 -0800

    Find permutations of given string

---
+++ Arrays/2. string_permutations.py
@@ -0,0 +1,9 @@
+# Find permutations of given string:
+# Python:
+
+from itertools import permutations
+str1 = "abc"
+for s in list(permutations(str1)):                 
+  print(s)
+
+Java: (Backtracking)
