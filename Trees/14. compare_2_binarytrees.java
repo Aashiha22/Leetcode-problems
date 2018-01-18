6867f079fbf8b1fcb68297facfe79b22f1fb4e59
Aashiha Priyadarshni <aashihapl@gmail.com>
2018-01-18 14:42:49 -0800

    Create 14. compare_2_binarytrees.java

---
+++ Trees/14. compare_2_binarytrees.java
@@ -0,0 +1,20 @@
+//Check if 2 binary trees are same or not
+
+class Solution 
+{
+    public boolean isSameTree(TreeNode p, TreeNode q) 
+    {
+        if(p == null && q == null)
+            return true;
+        else if(p == null || q == null)
+            return false;
+        else
+        {
+            boolean a = isSameTree(p.left, q.left);
+            boolean b = isSameTree(p.right, q.right);
+            if(a == false || b == false)
+                return false;
+            return p.val==q.val;
+        }
+    }
+}
