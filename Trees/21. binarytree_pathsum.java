6caa28cfcc3c09e0975b7995dc41fc27e2e39e20
Aashiha Priyadarshni <aashihapl@gmail.com>
2018-01-18 14:50:51 -0800

    check if binary tree has path sum

--- Trees/21. binarytree_pathsum.java
+++ Trees/21. binarytree_pathsum.java
@@ -1,4 +1,4 @@
-Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+/*Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
 For example:
 Given the below binary tree and sum = 22,
               5
@@ -9,7 +9,7 @@ Given the below binary tree and sum = 22,
          /  \      \
         7    2      1
 return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
-Code:
+Code: */
 
 class Solution 
 {
